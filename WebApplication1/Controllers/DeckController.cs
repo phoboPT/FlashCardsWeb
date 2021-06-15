f404ce831c179ebb76ec93ee7e50f63942fe2f1e
Ricardo Dias <42947771+rycardodias@users.noreply.github.com>
2021-06-15 20:46:29 +0100

    deck search fix

--- WebApplication1/Controllers/DeckController.cs
+++ WebApplication1/Controllers/DeckController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,9 +6,12 @@ using WebApplication1.Models.DatabaseContext;
 
 namespace WebApplication1.Controllers
 {
+    
+   
     public class DeckController : Controller
     {
         private ApplicationContext _context;
+
         public DeckController()
         {
             _context = new ApplicationContext();
@@ -22,7 +24,8 @@ namespace WebApplication1.Controllers
          
             
             var query = _context.DeckDetails.FromSqlRaw("SELECT de.* FROM \"Deck\" de, \"DisciplineUser\" du "
-                + "WHERE  de.\"disciplineKey\"= du.\"disciplineKey\" AND du.userkey = " + user.key+";").ToList();
+                + "WHERE  de.\"disciplineKey\"= du.\"disciplineKey\" AND du.userkey = " + user.key +
+                "AND (type = 1 OR (de.type = 0 AND de.\"userCreator\" = " + user.key + "))"+ ";").ToList();
             
             return View(query);
         }
